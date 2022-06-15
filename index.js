9cd4f3d73a784183a589e5358684b54f6c2ebc60
D068741 <a.chaurasia@sap.com>
2022-06-15 07:26:58 +0200

    sync

--- index.js
+++ index.js
@@ -2,30 +2,34 @@
 
 const express = require('express')
 var mysql = require('mysql');
-var edge = require('edge');
+require("msnodesqlv8");
 
 const app = express();
 const port = 3000
-var params = {
-    connectionString: "Server=YourServer;Database=YourDB;Integrated Security=True",
-    source: "SELECT TOP 20 * FROM _bvStockAndWhseItems"
-  };
-var getData = edge.func( 'sql', params);
- 
+  
+
+var con = new mysql.createConnection({
+    database: "mydb",
+    server: "localhost",
+    driver: "msnodesqlv8",
+    options: {
+        trustedConnection: true
+      }
+  });
+  
 app.get('/', (req, res) => {
     res.send('Hello World!')
   })
   
   app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-    getData(null, function (error, result) {
-        if (error) { console.log(error); return; }
-        if (result) {
-         console.log(result);
-        }
-        else {
-         console.log("No results");
-        }
+    con.connect(function(err) {
+        if (err) throw err;
+        con.query("SELECT * FROM customers", function (err, result, fields) {
+          if (err) throw err;
+          console.log(result);
+        });
       });
   })
 
+  // https://stackoverflow.com/questions/33709807/how-to-connect-to-sql-server-with-windows-authentication-from-node-js-using-mssq
